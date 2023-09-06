26170c017f17a463724506a0e4433b095e192d05
alicevalves <73171191+alicevalves@users.noreply.github.com>
2023-09-05 22:24:03 -0300

    [main] Iniciando integração com o banco de dados

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDZkQAdzSxGyMwHyQ68XkiQlAcb7J-xo7c",
+    authDomain: "api-controleanimais.firebaseapp.com",
+    projectId: "api-controleanimais",
+    storageBucket: "api-controleanimais.appspot.com",
+    messagingSenderId: "859359510343",
+    appId: "1:859359510343:web:e347ac0730db5bf0c71662",
+    measurementId: "G-S790NJZBH8"
+  };
+
+module.exports = firebaseConfig;
