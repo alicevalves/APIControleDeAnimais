67c54875b14548a7a654d57ae2c5db1e5c3241e9
alicevalves <73171191+alicevalves@users.noreply.github.com>
2023-09-10 20:24:46 -0300

    [main] Mudança no banco do firebase

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,14 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDZkQAdzSxGyMwHyQ68XkiQlAcb7J-xo7c",
-    authDomain: "api-controleanimais.firebaseapp.com",
-    projectId: "api-controleanimais",
-    storageBucket: "api-controleanimais.appspot.com",
-    messagingSenderId: "859359510343",
-    appId: "1:859359510343:web:e347ac0730db5bf0c71662",
-    measurementId: "G-S790NJZBH8"
-  };
+  apiKey: "AIzaSyDhWgYcNDsodel8HZgfdBEcsyCIel3CV14",
+  authDomain: "bora-103cc.firebaseapp.com",
+  databaseURL: "https://bora-103cc-default-rtdb.firebaseio.com",
+  projectId: "bora-103cc",
+  storageBucket: "bora-103cc.appspot.com",
+  messagingSenderId: "155887568657",
+  appId: "1:155887568657:web:e4320884e7cbb3d32eaeb0",
+  measurementId: "G-VT0BRPJQLK"
+};
+
 
 module.exports = firebaseConfig;
